f6ab1fcb76004930f51982e1ccc719a3af34f7dc
Sebastian Carlberg <carlberg.sebastian@gmail.com>
2020-01-26 19:32:00 +0100

    changed from manual pressing calculate to automatically updating slightly updated the ui from VStack to Sections with headers

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -19,6 +19,7 @@ struct ContentView: View {
     @State private var showingAlert = false
     
     let model = SleepCalculator()
+    
     static var defaultWakeTime: Date {
         var components = DateComponents()
         components.hour = 7
@@ -27,54 +28,7 @@ struct ContentView: View {
         return Calendar.current.date(from: components) ?? Date()
     }
     
-    var body: some View {
-        NavigationView {
-            Form {
-                VStack(alignment: .leading, spacing: 0) {
-                    Text("When do you want to wake up?")
-                        .font(.headline)
-                    DatePicker("Please enter a date", selection: $wakeUp, displayedComponents: .hourAndMinute)
-                    .labelsHidden()
-                }
-                
-                
-                
-                VStack(alignment: .leading, spacing: 0) {
-                    Text("Desired amount of sleep")
-                        .font(.headline)
-                    Stepper(value: $sleepAmount, in: 4...12, step: 0.25) {
-                        Text("\(sleepAmount, specifier: "%g") hours")
-                    }
-                }
-                
-                VStack(alignment: .leading, spacing: 0) {
-                    Text("Daily coffee intake")
-                        .font(.headline)
-                    Stepper(value: $coffeAmount, in: 1...20) {
-                        if (coffeAmount == 1) {
-                            Text("1 cup")
-                        } else {
-                            Text("\(coffeAmount) cups")
-                        }
-                    }
-                }
-                
-                .alert(isPresented: $showingAlert) {
-                    Alert(title: Text(alertTitle), message: Text(alertMessage), dismissButton: .default(Text("OK")))
-                }
-            }
-            .navigationBarTitle("BetterRest")
-            .navigationBarItems(trailing:
-                Button(action: calculateBedtime) {
-                    Text("Calculate")
-                }
-            )
-        }
-        
-        
-    }
-    
-    func calculateBedtime() {
+    var result: String {
         let components = Calendar.current.dateComponents([.hour, .minute], from: wakeUp)
         let hour = (components.hour ?? 0) * 60 * 60
         let minute = (components.minute ?? 0) * 60
@@ -83,15 +37,44 @@ struct ContentView: View {
             let prediction = try model.prediction(wake: (Double(hour + minute)), estimatedSleep: sleepAmount, coffee: Double(coffeAmount))
             let sleepTime = wakeUp - prediction.actualSleep
             let formatter = DateFormatter()
-            
+        
             formatter.timeStyle = .short
-            alertTitle = "Your ideal bedtime is..."
-            alertMessage = formatter.string(from: sleepTime)
-            showingAlert = true
+            
+            return formatter.string(from: sleepTime)
         } catch {
-            alertTitle = "Error"
-            alertMessage = "Sorry, there was a problem calculating your bedtime."
-            showingAlert = true
+            return "Sorry, there was a problem calculating your bedtime."
+        }
+    }
+    
+    var body: some View {
+        NavigationView {
+            Form {
+                Section(header: Text("When do you want to wake up?")) {
+                    DatePicker("Please enter a date", selection: $wakeUp, displayedComponents: .hourAndMinute)
+                    .labelsHidden()
+                }
+                Section(header: Text("Desired amount of sleep")) {
+                    Stepper(value: $sleepAmount, in: 4...12, step: 0.25) {
+                        Text("\(sleepAmount, specifier: "%g") hours")
+                    }
+                }
+                Section(header: Text("Daily coffee intake")) {
+                    Stepper(value: $coffeAmount, in: 1...20) {
+                        if (coffeAmount == 1) {
+                            Text("1 cup")
+                        } else {
+                            Text("\(coffeAmount) cups")
+                        }
+                    }
+                }
+                Section(header: Text("Your ideal bedtime is")) {
+                    Text("\(result)")
+                }
+                .alert(isPresented: $showingAlert) {
+                    Alert(title: Text(alertTitle), message: Text(alertMessage), dismissButton: .default(Text("OK")))
+                }
+            }
+            .navigationBarTitle("BetterRest")
         }
     }
 }
